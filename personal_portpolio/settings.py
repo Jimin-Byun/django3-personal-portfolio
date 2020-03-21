26c0e2b0514b502fd801ae1b955996b7fb0ae6da
Jimin Byun <jimin.stella@gmail.com>
2020-03-20 22:16:55 -0700

    new local settings:

--- personal_portpolio/settings.py
+++ personal_portpolio/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
